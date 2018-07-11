b8f13e968571ead31ab8ae23f27b90ab3c756b92
Stephen Sibaly <stephen.laval.sibaly@accenture.com>
2018-07-11 16:26:36 +0400

    docker4

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,17 +1,17 @@
-package com.accenture.controllers;
+package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/")
 public class HelloController {
 
 	
-	@GetMapping("")
+	@GetMapping("/hello")
 	public String sendToHellohtmlPage() {
 		return "Hello";
 	}
